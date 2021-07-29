2763428f0c4592ea1813df36d0fb0b18f7798066
khayalfarzi <xeyalferziyev9@gmail.com>
2021-07-29 19:32:35 +0300

    lesson 2

--- src/main/java/az/company/springbootq5/controller/HelloJavaController.java
+++ src/main/java/az/company/springbootq5/controller/HelloJavaController.java
@@ -1,7 +1,48 @@
 package az.company.springbootq5.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloJavaController {
+
+    private static final Map<String, String> MAP = new HashMap<>();
+
+    @PostConstruct
+    public void loadData() {
+        MAP.put("1", "111");
+        MAP.put("2", "222");
+        MAP.put("3", "333");
+        MAP.put("4", "444");
+        MAP.put("5", "555");
+        MAP.put("6", "666");
+    }
+
+    @GetMapping("/get/fin/{fin}")
+    public String getInfoByFin(@PathVariable("fin") String fin) {
+        return String.format("Your detail is : %s", MAP.get(fin));
+    }
+
+    @PostMapping("/post/{key}/{v}")
+    public void addInfo(@PathVariable("key") String key, @PathVariable("v") String value) {
+        MAP.put(key, value);
+    }
+
+    @PutMapping("/put/{key}/{v}")
+    public void updateInfo(@PathVariable("key") String key, @PathVariable("v") String value) {
+        MAP.put(key, value);
+    }
+
+    @PatchMapping("/patch/{key}/{v}")
+    public void updateInfoByPatch(@PathVariable("key") String key, @PathVariable("v") String value) {
+        MAP.put(key, value);
+    }
+
+    @DeleteMapping("/delete/{key}")
+    public void deleteInfo(@PathVariable("key") String key) {
+        MAP.remove(key);
+    }
 }
---
+++ src/main/java/az/company/springbootq5/model/Person.java
@@ -0,0 +1,6 @@
+package az.company.springbootq5.model;
+
+public class Person {
+    private Integer id;
+    private String name;
+}
