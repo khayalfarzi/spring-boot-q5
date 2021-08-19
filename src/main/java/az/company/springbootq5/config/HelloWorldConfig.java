e9bb9d69b3aab983c1784d76263925faf6dd26ed
khayalfarzi <xeyalferziyev9@gmail.com>
2021-08-19 19:46:42 +0300

    lesson 5

---
+++ src/main/java/az/company/springbootq5/config/HelloWorldConfig.java
@@ -0,0 +1,14 @@
+package az.company.springbootq5.config;
+
+import az.company.springbootq5.repository.HelloJavaRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWorldConfig {
+
+//    @Bean
+//    public HelloJavaRepository getHelloJavaRepository() {
+//        return new HelloJavaRepository();
+//    }
+}
---
+++ src/main/java/az/company/springbootq5/model/User.java
@@ -0,0 +1,23 @@
+package az.company.springbootq5.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users", schema = "q5")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+}
